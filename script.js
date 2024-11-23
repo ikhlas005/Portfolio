6c716214f56ec753d791eec0cc3e99d719ed8794
ikhlas005 <innocentkharal555@gmail.com>
2024-11-23 18:36:09 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings: ["Graphic Designer", "Web Developer", "Video Editor", "Microsoft Office"],
+    typeSpeed: 90,
+    backSpeed: 60,
+    loop: true
+});
